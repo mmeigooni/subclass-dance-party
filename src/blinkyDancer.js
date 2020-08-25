1aae4b53cd50d35aef20128efd9b4bdabdf2550c
David <davidmcruz1125@gmail.com>
2020-08-24 19:33:59 -0500

    Apply psudoclassical style to blinkyDancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,25 +1,10 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
 
-  // MakeBlinkyDancer.call(this, )
+  this.oldStep = this.step;
+};
 
-  // we have built an instance of the dancer class {$node, step, setPosition}
-
-
-  // prevents overwrite from new dancers
-  // permenantly assigns a step method based off the instance
-  var oldStep = blinkyDancer.step; // saves 'schedule' blinkyDancer
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-
-    oldStep(); // evaluates to a placement of a red dot after x seconds
-
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();// toggles the immage away
-  };
-
-  return blinkyDancer;
+MakeBlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  this.$node.toggle();
 };
